001ba87c12242ba2324cf1db372ca05606312caa
moka0690 <madradz39@gmail.com>
2024-02-21 20:40:10 +0100

    solveSolving exercise 2

--- mian.js
+++ mian.js
@@ -1,5 +1,21 @@
 
 console.log([(10*(20-15)*3)-190]*10+400)
+let num = 3;
 
-   
- ; 
+// Solution One
+console.log( num + num); // 6
+
+// Solution Two
+console.log( num + true + true + true); // 6
+
+// Soultion Three
+console.log( num * [ true+ true]); // 6
+
+// Soultion Four
+console.log( [num / num] * (true+ true+true+ true+true+ true)); // 6
+
+// Solution Five
+console.log(  (num * num + num) / (true+true)); // 6
+
+// Solution Six
+console.log((num*num+num+num+num)/(num)); // 6
